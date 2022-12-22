9188c447b88344c78a0442b2ca7aac5fb9c00a72
zoushuang <cn-zoushuang@qq.com>
2022-12-22 22:49:49 +0800

    chore: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,47 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    new copy({
-      targets: [
-        {
-          src: "src/workers/*",
-          dest: "public/workers",
-        },
-      ],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const config = loadEnv(mode, "./");
+  return {
+    base: config.VITE_BASIC || "./",
+    build: {
+      outDir: "docs",
     },
-  },
-  server: {
-    proxy: {
-      "/schemas": {
-        target: "http://localhost:8001/",
-        changeOrigin: true,
+    plugins: [
+      vue(),
+      new copy({
+        targets: [
+          {
+            src: "src/workers/*",
+            dest: "public/workers",
+          },
+        ],
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-  },
-  optimizeDeps: {
-    include: [
-      `monaco-editor/esm/vs/language/json/json.worker`,
-      `monaco-editor/esm/vs/editor/editor.worker`,
-    ],
-  },
+    server: {
+      proxy: {
+        "/schemas": {
+          target: "http://localhost:8001/",
+          changeOrigin: true,
+        },
+      },
+    },
+    optimizeDeps: {
+      include: [
+        `monaco-editor/esm/vs/language/json/json.worker`,
+        `monaco-editor/esm/vs/editor/editor.worker`,
+      ],
+    },
+  };
 });
